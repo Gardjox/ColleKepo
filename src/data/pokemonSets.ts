03f78c5275f1655050f14d1100dd5aa9b8e5a6ee
Gardjox <Gard_jax@outlook.fr>
2026-05-04 11:34:03 +0200

    Add files via upload

--- src/data/pokemonSets.ts
+++ src/data/pokemonSets.ts
@@ -221,6 +221,148 @@ export const POKEMON_SERIES: PokemonSeries[] = [
     {
         id: 'promos',
         name: 'Promos',
-        sets: []
+        sets: [
+            { id: 'ToT2024', name: 'Trick or Trade 2024', shortName: 'ToT2024' },
+            { id: 'ToT2023', name: 'Trick or Trade 2023', shortName: 'ToT2023' },
+            { id: 'ToT2022', name: 'Trick or Trade 2022', shortName: 'ToT2022' },
+            { id: 'CLB', name: 'Classic Blastoise & Suicune', shortName: 'CLB' },
+            { id: 'CLC', name: 'Classic Charizard & Ho-Oh', shortName: 'CLC' },
+            { id: 'CLV', name: 'Classic Venusaur & Lugia', shortName: 'CLV' },
+            { id: 'DetecP', name: 'Détective Pikachu', shortName: 'DetecP' },
+            { id: 'Futsal', name: 'Futsal', shortName: 'Futsal' },
+            { id: 'ProAl', name: 'Promos Alternative', shortName: 'ProAl' },
+            { id: 'Rumble', name: 'Rumble', shortName: 'Rumble' },
+            { id: 'Creator', name: 'PokéCard Creator Pack', shortName: 'Creator' },
+            { id: 'South', name: 'Southern Island Collection', shortName: 'South' },
+            { id: 'NoNum', name: 'Promos Non Numérotées', shortName: 'NoNum' },
+            { id: 'BestPro', name: 'Wizards Best Promo', shortName: 'BestPro' },
+            { id: 'BoxTop', name: 'Box Topper', shortName: 'BoxTop' }
+        ]
+    },
+    {
+        id: 'ligue-pokemon',
+        name: 'Ligue Pokémon',
+        sets: [
+            { id: 'Recomp', name: 'Pack Récompense', shortName: 'Récomp' },
+            { id: 'Pop9', name: 'Pop9', shortName: 'Pop9' },
+            { id: 'Pop8', name: 'Pop8', shortName: 'Pop8' },
+            { id: 'Pop7', name: 'Pop7', shortName: 'Pop7' },
+            { id: 'Pop6', name: 'Pop6', shortName: 'Pop6' },
+            { id: 'Pop5', name: 'Pop5', shortName: 'Pop5' },
+            { id: 'Pop4', name: 'Pop4', shortName: 'Pop4' },
+            { id: 'Pop3', name: 'Pop3', shortName: 'Pop3' },
+            { id: 'Pop2', name: 'Pop2', shortName: 'Pop2' },
+            { id: 'Pop1', name: 'Pop1', shortName: 'Pop1' }
+        ]
+    },
+    {
+        id: 'jumbo',
+        name: 'Jumbo',
+        sets: [
+            { id: 'Jumbo', name: 'Jumbo', shortName: 'Jumbo' }
+        ]
+    },
+    {
+        id: 'kit-dresseur',
+        name: 'Kit du Dresseur',
+        sets: [
+            { id: 'Dark', name: 'Académie de Combat Darkrai', shortName: 'Dark' },
+            { id: 'Pika24', name: 'Académie de Combat Pikachu 2024', shortName: 'Pika24' },
+            { id: 'Carma', name: 'Académie de Combat Carmadura', shortName: 'Carma' },
+            { id: 'Evoli', name: 'Académie de Combat Évoli', shortName: 'évoli' },
+            { id: 'Pika22', name: 'Académie de Combat Pikachu 2022', shortName: 'Pika22' },
+            { id: 'Pyro', name: 'Académie de Combat Pyrobut', shortName: 'Pyro' },
+            { id: 'MewTwo', name: 'Académie de Combat Mewtwo', shortName: 'MewTwo' },
+            { id: 'Pika20', name: 'Académie de Combat Pikachu 2020', shortName: 'Pika20' },
+            { id: 'Drac', name: 'Académie de Combat Dracaufeu', shortName: 'Drac' },
+            { id: 'KDSabl', name: 'Kit Dresseur Sablaireau D\'Alola', shortName: 'KDSabl' },
+            { id: 'KDFeuna', name: 'Kit Dresseur Feunard D\'Alola', shortName: 'KDFeuna' },
+            { id: 'KDRaich', name: 'Kit Dresseur Raichu D\'Alola', shortName: 'KDRaich' },
+            { id: 'KDLouga', name: 'Kit Dresseur Lougaroc', shortName: 'KDLouga' },
+            { id: 'KDSuicu', name: 'Kit Dresseur Suicune', shortName: 'KDSuicu' },
+            { id: 'KDPika', name: 'Kit Dresseur Pikachu Catcheur', shortName: 'KDPika' },
+            { id: 'Tios15', name: 'Kit Dresseur Latios 2015', shortName: 'Tios15' },
+            { id: 'Tias15', name: 'Kit Dresseur Latias 2015', shortName: 'Tias15' },
+            { id: 'Gdoudou', name: 'Kit Dresseur Grodoudou', shortName: 'Gdoudou' },
+            { id: 'Sclp', name: 'Kit Dresseur Scalproie', shortName: 'Sclp' },
+            { id: 'Bruyv', name: 'Kit Dresseur Bruyverne', shortName: 'Bruyv' },
+            { id: 'Nymph', name: 'Kit Dresseur Nymphali', shortName: 'Nymph' },
+            { id: 'Taupe', name: 'Kit Dresseur Minotaupe', shortName: 'Taupe' },
+            { id: 'Zoroa', name: 'Kit Dresseur Zoroark', shortName: 'Zoroa' },
+            { id: 'Raich', name: 'Kit Dresseur Raichu', shortName: 'Raich' },
+            { id: 'Leviat', name: 'Kit Dresseur Léviator', shortName: 'Léviat' },
+            { id: 'Manaph', name: 'Kit Dresseur Manaphy', shortName: 'Manaph' },
+            { id: 'Lucar', name: 'Kit Dresseur Lucario', shortName: 'Lucar' },
+            { id: 'Posip', name: 'Kit Dresseur Posipi', shortName: 'Posip' },
+            { id: 'Negap', name: 'Kit Dresseur Négapi', shortName: 'Négap' },
+            { id: 'Tios06', name: 'Kit Dresseur Latios 2006', shortName: 'Tios06' },
+            { id: 'Tias06', name: 'Kit Dresseur Latias 2006', shortName: 'Tias06' }
+        ]
+    },
+    {
+        id: 'world-championships',
+        name: 'World Championships',
+        sets: [
+            { id: 'WCS24', name: 'World Championships 2024', shortName: 'WCS24' },
+            { id: 'WCS23', name: 'World Championships 2023', shortName: 'WCS23' },
+            { id: 'WCS22', name: 'World Championships 2022', shortName: 'WCS22' },
+            { id: 'WCS19', name: 'World Championships 2019', shortName: 'WCS19' },
+            { id: 'WCS18', name: 'World Championships 2018', shortName: 'WCS18' },
+            { id: 'WCS17', name: 'World Championships 2017', shortName: 'WCS17' },
+            { id: 'WCS16', name: 'World Championships 2016', shortName: 'WCS16' },
+            { id: 'WCS15', name: 'World Championships 2015', shortName: 'WCS15' },
+            { id: 'WCS14', name: 'World Championships 2014', shortName: 'WCS14' },
+            { id: 'WCS13', name: 'World Championships 2013', shortName: 'WCS13' },
+            { id: 'WCS12', name: 'World Championships 2012', shortName: 'WCS12' },
+            { id: 'WCS11', name: 'World Championships 2011', shortName: 'WCS11' },
+            { id: 'WCS10', name: 'World Championships 2010', shortName: 'WCS10' },
+            { id: 'WCS09', name: 'World Championships 2009', shortName: 'WCS09' },
+            { id: 'WCS08', name: 'World Championships 2008', shortName: 'WCS08' },
+            { id: 'WCS07', name: 'World Championships 2007', shortName: 'WCS07' },
+            { id: 'WCS06', name: 'World Championships 2006', shortName: 'WCS06' },
+            { id: 'WCS05', name: 'World Championships 2005', shortName: 'WCS05' },
+            { id: 'WCS04', name: 'World Championships 2004', shortName: 'WCS04' }
+        ]
+    },
+    {
+        id: 'mcdonalds',
+        name: 'Mcdonald\'s',
+        sets: [
+            { id: 'Mcdo24', name: 'Promo McDonald\'s 2024', shortName: 'Mcdo24' },
+            { id: 'Mcdo23', name: 'Promo McDonald\'s 2023', shortName: 'Mcdo23' },
+            { id: 'Mcdo22', name: 'Promo McDonald\'s 2022', shortName: 'Mcdo22' },
+            { id: 'Mcdo21', name: 'Promo McDonald\'s 2021', shortName: 'Mcdo21' },
+            { id: 'Mcdo19', name: 'Promo McDonald\'s 2019', shortName: 'Mcdo19' },
+            { id: 'Mcdo19USA', name: 'Promo McDonald\'s 2019USA', shortName: 'Mcdo19USA' },
+            { id: 'Mcdo18', name: 'Promo McDonald\'s 2018', shortName: 'Mcdo18' },
+            { id: 'Mcdo18USA', name: 'Promo McDonald\'s 2018USA', shortName: 'Mcdo18USA' },
+            { id: 'Mcdo17', name: 'Promo McDonald\'s 2017', shortName: 'Mcdo17' },
+            { id: 'Mcdo16', name: 'Promo McDonald\'s 2016', shortName: 'Mcdo16' },
+            { id: 'Mcdo15', name: 'Promo McDonald\'s 2015', shortName: 'Mcdo15' },
+            { id: 'Mcdo14', name: 'Promo McDonald\'s 2014', shortName: 'Mcdo14' },
+            { id: 'Mcdo13', name: 'Promo McDonald\'s 2013', shortName: 'Mcdo13' },
+            { id: 'Mcdo12', name: 'Promo McDonald\'s 2012', shortName: 'Mcdo12' },
+            { id: 'Mcdo11', name: 'Promo McDonald\'s 2011', shortName: 'Mcdo11' }
+        ]
+    },
+    {
+        id: 'divers',
+        name: 'Divers',
+        sets: [
+            { id: 'Fetes25', name: 'Calendrier des fêtes 2025', shortName: 'Fêtes25' },
+            { id: 'Fetes24', name: 'Calendrier des fêtes 2024', shortName: 'Fêtes24' },
+            { id: 'Fetes23', name: 'Calendrier des fêtes 2023', shortName: 'Fêtes23' },
+            { id: 'Fetes22', name: 'Calendrier des fêtes 2022', shortName: 'Fêtes22' },
+            { id: 'Fetes10', name: 'Calendrier de l\'avent Pokémon 2010 Uberraschungskalender', shortName: 'Fêtes10' },
+            { id: 'Fetes09', name: 'Calendrier de l\'avent Pokémon 2009', shortName: 'Fêtes09' },
+            { id: 'Energy', name: 'Énergies', shortName: 'Energy' },
+            { id: 'Art', name: 'Pokémon Art Academy', shortName: 'Art' },
+            { id: 'Defi', name: 'Le Premier Défi', shortName: 'Défi' },
+            { id: 'PikaWC2000', name: 'Pikachu World Collection', shortName: 'PikaWC2000' },
+            { id: 'Topps', name: 'Topps', shortName: 'opps' },
+            { id: 'PikaWC10', name: 'Pikachu World Collection 2010', shortName: 'PikaWC10' },
+            { id: 'Lamin06', name: 'LaminCards 2006', shortName: 'Lamin06' },
+            { id: 'Lamin05', name: 'LaminCards 2005', shortName: 'Lamin05' }
+        ]
     }
 ];
